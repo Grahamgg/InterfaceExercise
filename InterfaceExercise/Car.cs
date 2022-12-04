4b8739f5862bf5d212885c826fffba1c1c8cd85b
Graham Galloway <Grahamgalloway0@gmail.com>
2022-12-04 10:26:13 -0500

    intial commit

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    public class Car : IVehicle, ICompany
+    {
+        public Car()
+        {
+
+        }
+        public double EngineSize { get; set; } = 4.6;
+        public string Make { get; set; } = "Ford";
+        public string Model { get; set; } = "Generic";
+        public int SeatCount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Generic";
+        public string Motto { get; set; } = "Generic";
+        public bool HasChangedGears { get; set; }
+        public int LazerSize { get; set; } = 20;
+        public int ShieldSize { get; set; } = 10;
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward . . .");
+        }
+        public void Reverse()
+        {
+            if(HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing . . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we changed gears");
+            }
+            
+        }
+        public void Park()
+        {
+           
+        }
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+    }
+}
---
+++ InterfaceExercise/SUV.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    public class SUV: IVehicle , ICompany 
+    {
+        public SUV()
+        {
+
+        }
+        public double EngineSize { get; set; } = 8;
+        public string Make { get; set; } = "Toyota";
+        public string Model { get; set; } = "Highlander";
+        public int SeatCount { get; set; } = 7;
+        public string CompanyName { get; set; } = "Toyota";
+        public string Motto { get; set; } = "Let's Go Places";
+        public bool HasChangedGears { get; set; }
+
+        public int GatlingGunSize { get; set; } = 10;
+        public int ArmorRating { get; set; } = 10;
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward . . .");
+        }
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing . . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we changed gears");
+            }
+
+        }
+        public void Park()
+        {
+            Console.WriteLine();
+        }
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+    }
+}
